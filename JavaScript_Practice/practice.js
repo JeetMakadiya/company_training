6b1a07139f4ca82ba5fd141b55f031ff4b0907fd
JeetMakadiya <jeetmakadiya01@gmail.com>
2021-12-28 11:37:17 +0530

    simple commit 1

--- JavaScript_Practice/practice.js
+++ JavaScript_Practice/practice.js
@@ -14,3 +14,28 @@ for(let i=0;i<arr.length;i++){
     element2 += "<li>"+arr[i]+"</li>";
 }
 element1.innerHTML = element2;
+
+// select theme code
+let select = document.getElementById('select_menu');
+let html = document.querySelector('html');
+
+function changeTheme(bgColor, fontColor){
+    html.style.backgroundColor = bgColor;
+    html.style.color = fontColor;
+}
+
+select.addEventListener('change',() => (select.value === "dark_mode")?changeTheme('black','white'):changeTheme('lightblue','black'));
+
+// simple calender
+let select2 = document.getElementById('select_month');
+let showMonth = document.getElementById('show_month');
+let dayList = document.getElementById('day_list');
+
+function createCalender(month, days){
+    showMonth.textContent = month;
+    let listItem = document.createElement('li');
+}
+
+select2.addEventListener('change',() => {
+
+});
